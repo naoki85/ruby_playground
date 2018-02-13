e4396c5c78620980605dc9431071313b03027ed8
naoki85 <naoki.yoneyama.85@gmail.com>
2018-02-13 23:28:05 +0900

    Add btn helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+  # For index, show, back
+  def default_btn_class
+    'waves-effect btn grey lighten-5 black-text'
+  end
+
+  # For new, create
+  def create_btn_class
+    'waves-effect waves-light btn green accent-2 black-text'
+  end
+
+  # For edit, update
+  def update_btn_class
+    'waves-effect waves-light btn orange accent-2 black-text'
+  end
+
+  # For delete, alert
+  def danger_btn_class
+    'waves-effect waves-light btn red accent-2 black-text'
+  end
+
+  # For signup, login
+  def session_btn_class
+    'waves-effect waves-teal btn'
+  end
 end
