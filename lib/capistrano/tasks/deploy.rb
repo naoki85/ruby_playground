37e849e2201f73d9b4da38da9be0e3766b67fa8f
naoki85 <naoki.yoneyama.85@gmail.com>
2018-02-21 07:26:47 +0900

    #5 SitemapのCapistranoタスクを作成

---
+++ lib/capistrano/tasks/deploy.rb
@@ -0,0 +1,33 @@
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      branch = fetch(:branch)
+      unless `git rev-parse HEAD` == `git rev-parse #{branch}`
+        puts "WARNING: HEAD is not the same as origin/#{branch}"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'unicorn:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'unicorn:restart'
+    end
+  end
+
+  before :starting,     :check_revision
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
---
+++ lib/capistrano/tasks/sitemap.rb
@@ -0,0 +1,14 @@
+namespace :sitemap do
+  desc 'Sitemap refresh'
+  task :refresh do
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env) do
+          execute :rake, 'sitemap:refresh'
+        end
+      end
+    end
+  end
+end
+
+before 'deploy:finishing', 'sitemap:refresh'
