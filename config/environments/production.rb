8c220b5242f74d0e81cb48d38ef4b388dc66567a
naoki85 <naoki.yoneyama.85@gmail.com>
2018-03-11 21:23:52 +0900

    #13 Log Lotate

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,6 +78,7 @@ Rails.application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
+  config.logger = Logger.new('log/production.log', 14, 10 * 1024 * 1024)
 
   # Use a different logger for distributed setups.
   # require 'syslog/logger'
