d739c3d5194be1cebf19fcab5e8312341a1a7ed9
naoki85 <naoki.yoneyama.85@gmail.com>
2018-04-08 15:32:33 +0900

    #30 Add chart.js

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -2,8 +2,23 @@ class TopController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @publishers = Publisher.all
-    @books = Book.union_select_recent_each_publisher(@publishers.pluck(:id)).group_by(&:publisher_id)
+    if user_signed_in?
+      book_ids = UserBook.all.pluck(:book_id)
+      book_categories = BookCategory.all
+      books_by_category_id = Book.where(id: book_ids).group('book_category_id').count
+      @book_by_category = {}
+      book_categories.each do |book_category|
+        # Unspecified の場合は next
+        next if book_category.id == 0
+        @book_by_category[book_category.name] = 0
+        if books_by_category_id.key?(book_category.id)
+          @book_by_category[book_category.name] = books_by_category_id[book_category.id]
+        end
+      end
+    else
+      @publishers = Publisher.all
+      @books = Book.union_select_recent_each_publisher(@publishers.pluck(:id)).group_by(&:publisher_id)
+    end
   end
 
   def user_policy
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,10 @@ FactoryBot.create(:publisher, name: '講談社', class_name: 'Crawler::KodansyaC
 FactoryBot.create(:publisher, name: '集英社', class_name: 'Webdriver::ShueisyaCrawler')
 
 BookCategory.delete_all
-FactoryBot.create(:book_category, id: 0, name: '指定なし')
+FactoryBot.create(:book_category, id: 0, name: 'Unspecified')
+FactoryBot.create(:book_category, id: 1, name: 'Comic')
+FactoryBot.create(:book_category, id: 2, name: 'Novel')
+FactoryBot.create(:book_category, id: 3, name: 'Business')
+FactoryBot.create(:book_category, id: 4, name: 'Hobby')
+FactoryBot.create(:book_category, id: 5, name: 'Education')
+FactoryBot.create(:book_category, id: 6, name: 'Other')
