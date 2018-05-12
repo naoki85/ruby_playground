b3092c4834ca041ba318022e40ff37c34a96bd96
naoki85 <naoki.yoneyama.85@gmail.com>
2018-05-12 21:54:54 +0900

    Uninstall active decorator

--- app/models/user.rb
+++ app/models/user.rb
@@ -57,6 +57,34 @@ class User < ApplicationRecord
     save!
   end
 
+  # @return boolean
+  def avatar_image?
+    image.attached? || image_url.present?
+  end
+
+  # @return string
+  def avatar_image_path(**options)
+    if image.attached?
+      if options.present? && options.key?(:resize)
+        url_for(image.variant(resize: options[:resize]))
+      else
+        url_for(image)
+      end
+    elsif image_url.present?
+      image_url
+    else
+      'https://s3-ap-northeast-1.amazonaws.com/bookrecorder-image/commons/default_user_icon.png'
+    end
+  end
+
+  def name
+    if username.present?
+      username
+    else
+      'No name'
+    end
+  end
+
   # @return bool
   def from_social?
     provider.present? && uid.present?
