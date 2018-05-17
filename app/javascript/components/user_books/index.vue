e1c6c1d1b3b85c60a354f901144d3e532dd1bb76
naoki85 <naoki.yoneyama.85@gmail.com>
2018-05-17 22:08:16 +0900

    Convert UserBooks#index

--- app/javascript/components/user_books/index.vue
+++ app/javascript/components/user_books/index.vue
@@ -1,39 +1,35 @@
 <template>
-  <div class="container">
-    <h1>本棚</h1>
-    <p v-if="isDeleted" class="alert alert-success">削除しました。</p>
-    <div v-for="book in user.books" class="row">
-      <div class="col s12">
-        <div class="card horizontal">
-          <div class="card-image">
-            <img :src="book.image_url" :alt="book.title">
-          </div>
-          <div class="card-stacked">
-            <div class="card-content">
-              <p>{{ book.title }}</p>
-              <p>{{ book.book_category }}</p>
+  <v-container fluid>
+    <div class="display-1">本棚</div>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 offset-sm3 v-for="book in user.books" :key="book.title">
+        <v-card>
+          <v-card-media :src="book.image_url" height="200px" :alt="book.title">
+          </v-card-media>
+          <v-card-title primary-title>
+            <div>
+              <h3 class="headline mb-0">{{ book.title }}</h3>
+              <div>{{ book.book_category }}</div>
             </div>
-            <div class="card-action">
-              <router-link :to="'/books/' + book.id" class="waves-effect btn grey lighten-5 black-text">確認</router-link>
-              <button @click="deleteUserBook(book.id)" class="waves-effect waves-light btn red accent-3">削除</button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn flat color="orange" @click="redirectDetailPage(book.detail_page_url)">公式ページへ</v-btn>
+            <v-btn flat color="red" @click="deleteUserBook(book.id)">本棚から削除</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
   import request from '../../utils/requests'
-  import { mapState } from 'vuex'
-  import loading from '../commons/loading'
+  import { mapState, mapActions } from 'vuex'
 
   export default {
     data: function() {
       return {
-        user: [],
-        isDeleted: false
+        user: []
       }
     },
     computed: {
@@ -42,11 +38,17 @@
       ])
     },
     mounted: function() {
-      this.showLoading();
+      this.loading();
       this.fetchUser(this.userId);
-      this.hideLoading();
+      this.finish();
     },
     methods: {
+      ...mapActions('loader', [
+        'loading', 'finish'
+      ]),
+      ...mapActions('alert', [
+        'showSuccessAlert', 'showErrorAlert'
+      ]),
       fetchUser: function(userId) {
         request.get('/v1/users/' + userId, {}).then((response) => {
           this.user = response.data.user;
@@ -55,20 +57,26 @@
         });
       },
       deleteUserBook: function(bookId) {
-        this.showLoading();
-        var authenticateToken = localStorage.getItem('bookRecorderAuthenticationToken');
+        this.loading();
         request.delete('/v1/user_books', { params: { book_id: bookId }, auth: true })
             .then((response) => {
-          this.isDeleted = true;
+          this.showSuccessAlert({
+            message: '削除が完了しました'
+          });
           var userId = this.userId;
           this.fetchUser(userId);
         }, (error) => {
+          this.showErrorAlert({
+            message: '削除が失敗しました'
+          });
           console.log(error);
         });
-        this.hideLoading();
+        this.finish();
+      },
+      redirectDetailPage(url) {
+        location.href = url;
       }
-    },
-    mixins: [loading]
+    }
   }
 </script>
 
