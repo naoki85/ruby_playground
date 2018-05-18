bbda1463d9325928ef10e30bf7861f17f0dc4db0
naoki85 <naoki.yoneyama.85@gmail.com>
2018-05-18 23:53:26 +0900

    Deploy in CircleCI

---
+++ config/deploy/production.rb
@@ -0,0 +1,15 @@
+set :rails_env, 'production'
+set :unicorn_rack_env, 'deployment'
+
+set :branch, ENV['BRANCH_NAME'] || 'master'
+
+# role :app, %w{user@example.com}
+# role :web, %w{user@example.com}
+# role :db,  %w{user@example.com}
+server '13.231.79.158', user: 'naoki_yoneyama', roles: %w{web app db}
+
+# For Ridgepole
+set :ridgepole_env, fetch(:rails_env)
+set :ridgepole_roles, :db
+set :ridgepole_schema_file, File.join(current_path, 'db/schemas', 'Schemafile')
+set :ridgepole_config_file, File.join(current_path, 'config', 'database.yml')
