4f32cae60bf247fffefd0f57bebdc98303177f05
naoki85 <naoki.yoneyama.85@gmail.com>
2018-05-27 14:37:12 +0900

    Add chart.js

---
+++ app/javascript/components/commons/line_chart.vue
@@ -0,0 +1,54 @@
+<script>
+import {Line} from 'vue-chartjs'
+
+export default {
+  extends: Line,
+  data () {
+    return {
+      datacollection: {
+        //Data to be represented on x-axis
+        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#1DE9B6',
+            pointBackgroundColor: 'white',
+            borderWidth: 1,
+            pointBorderColor: '#004D40',
+            //Data to be represented on y-axis
+            data: [40, 20, 30, 50, 90, 10, 20, 40, 50, 70, 90, 100]
+          }
+        ]
+      },
+      //Chart.js options that controls the appearance of the chart
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: true
+            }
+          }],
+          xAxes: [ {
+            gridLines: {
+              display: false
+            }
+          }]
+        },
+        legend: {
+          display: true,
+          labels: false
+        },
+        responsive: true,
+        maintainAspectRatio: false
+      }
+    }
+  },
+  mounted () {
+    //renderChart function renders the chart with the datacollection and options object.
+    this.renderChart(this.datacollection, this.options)
+  }
+}
+</script>
--- app/javascript/components/users/show.vue
+++ app/javascript/components/users/show.vue
@@ -8,21 +8,37 @@
         <div class="display-1">{{ user.name }}</div>
       </v-flex>
     </v-layout>
-    <v-layout row wrap>
-      <v-flex xs12 sm6 offset-sm3 v-for="book in user.books" :key="book.title">
-        <v-card>
-          <v-card-media :src="book.image_url" height="200px" :alt="book.title">
-          </v-card-media>
-          <v-card-title primary-title>
-            <div>
-              <h3 class="headline mb-0">{{ book.title }}</h3>
-              <div>{{ book.book_category }}</div>
-            </div>
-          </v-card-title>
-          <v-card-actions>
-            <v-btn flat color="orange" @click="redirectDetailPage(book.detail_page_url)">公式ページへ</v-btn>
-            <!--<v-btn flat color="orange">Explore</v-btn>-->
-          </v-card-actions>
+
+    <line-chart></line-chart>
+
+    <v-layout row wrap class="mt-default">
+      <v-flex xs12 md6 v-for="book in user.books" :key="book.id">
+        <v-card color="" class="black--text">
+          <v-container fluid grid-list-lg>
+            <v-layout row>
+              <v-flex xs7>
+                <div>
+                  <div class="headline">{{ book.title }}</div>
+                  <div>{{ book.book_category }}</div>
+                </div>
+              </v-flex>
+              <v-flex xs5>
+                <router-link :to="'/books/' + book.id">
+                  <v-card-media
+                      :src="book.image_url"
+                      height="125px"
+                      :alt="book.title"
+                      contain
+                  ></v-card-media>
+                </router-link>
+              </v-flex>
+            </v-layout>
+            <v-layout row>
+              <v-card-actions>
+                <v-btn flat color="orange" @click="redirectDetailPage(book.detail_page_url)">公式ページへ</v-btn>
+              </v-card-actions>
+            </v-layout>
+          </v-container>
         </v-card>
       </v-flex>
     </v-layout>
@@ -32,8 +48,12 @@
 <script>
   import request from '../../utils/requests'
   import { mapActions } from 'vuex'
+  import lineChart from '../commons/line_chart'
 
   export default {
+    components: {
+      'line-chart': lineChart
+    },
     data: function() {
       return {
         user: []
