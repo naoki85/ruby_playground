fd3306dba9f5f6ed60ff0bee442b1de16a9225cb
naoki85 <naoki.yoneyama.85@gmail.com>
2018-07-22 23:05:04 +0900

    Remove function redirect to blog.bookrecorder

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,4 @@
 class PostsController < ApplicationController
-  before_action :check_host
 
   def index
     posts = Post.select(:title).released.order('published_at DESC').limit(5)
@@ -8,15 +7,5 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
-  end
-
-  private
-
-  def check_host
-    if Rails.env.production? && request.host == 'bookrecorder.net'
-      redirect_url = request.original_url
-      redirect_url = redirect_url.gsub(/bookrecorder.net/, 'blog.bookrecorder.net')
-      redirect_to redirect_url
-    end
   end
 end
