b3e786fc0c02c9f40b0ebbb5f354b928aa2b9b92
naoki85 <naoki.yoneyama.85@gmail.com>
2018-09-22 20:21:32 +0900

    Remove layout components and alert component

--- app/javascript/store/store.js
+++ app/javascript/store/store.js
@@ -1,13 +1,11 @@
 import Vue from 'vue/dist/vue.esm.js'
 import Vuex from 'vuex'
-import alert from './modules/alert'
-import auth from './modules/auth'
 import loader from './modules/loader'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   modules: {
-    auth, loader, alert
+    loader
   }
 })
