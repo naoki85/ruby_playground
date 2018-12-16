e3668f6e8abf91765e26144bd20704757e3b4c73
naoki85 <naoki.yoneyama.85@gmail.com>
2018-12-16 22:11:09 +0900

    Remove cron tasks for scraping

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,14 +3,6 @@ set :output, 'log/crontab.log'
 rails_env = ENV['RAILS_ENV'] || :development
 set :environment, rails_env
 
-every 1.day, :at => '4:00 am' do
-  runner "Tasks::DailyCrawl.execute", output: { error: 'log/cron_error.log' }
-end
-
-every 1.day, :at => '5:00 am' do
-  runner "Tasks::PickupBookCategories.execute", output: { error: 'log/cron_error.log' }
-end
-
 every 1.day, :at => '6:00 am' do
   rake "sitemap:refresh"
 end
