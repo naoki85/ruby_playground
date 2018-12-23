f4b8b28ddcdd81a4389ec629641522bb38089646
naoki85 <naoki.yoneyama.85@gmail.com>
2018-12-23 22:39:38 +0900

    Replace recommended book

--- app/javascript/components/commons/recommend_books.vue
+++ app/javascript/components/commons/recommend_books.vue
@@ -23,6 +23,11 @@
       return {
         recommendBooks: [
           {
+            bookLink: 'https://www.amazon.co.jp/gp/product/4797382228/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=4797382228&linkCode=as2&tag=naoki850c-22&linkId=91c6e0d453dff73ccb50e49212dd97fd',
+            bookImg: '//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=4797382228&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=naoki850c-22',
+            btnImg: '//ir-jp.amazon-adsystem.com/e/ir?t=naoki850c-22&l=am2&o=9&a=4797382228'
+          },
+          {
             bookLink: 'https://www.amazon.co.jp/gp/product/4873118042/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=4873118042&linkCode=as2&tag=naoki850c-22&linkId=2f0832d512b94dbffb202cba2ca930e7',
             bookImg: '//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=4873118042&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=naoki850c-22',
             btnImg: '//ir-jp.amazon-adsystem.com/e/ir?t=naoki850c-22&l=am2&o=9&a=4873118042'
@@ -36,11 +41,6 @@
             bookLink: 'https://www.amazon.co.jp/gp/product/4094065369/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=4094065369&linkCode=as2&tag=naoki850c-22&linkId=f91ee3c1bbbdaf66b9e4152e615451d6',
             bookImg: '//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=4094065369&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=naoki850c-22',
             btnImg: '//ir-jp.amazon-adsystem.com/e/ir?t=naoki850c-22&l=am2&o=9&a=4094065369'
-          },
-          {
-            bookLink: 'https://www.amazon.co.jp/gp/product/4309227368/ref=as_li_tl?ie=UTF8&camp=247&creative=1211&creativeASIN=4309227368&linkCode=as2&tag=naoki850c-22&linkId=13df1c7f982d28530f989515303aab97',
-            bookImg: '//ws-fe.amazon-adsystem.com/widgets/q?_encoding=UTF8&MarketPlace=JP&ASIN=4309227368&ServiceVersion=20070822&ID=AsinImage&WS=1&Format=_SL250_&tag=naoki850c-22',
-            btnImg: '//ir-jp.amazon-adsystem.com/e/ir?t=naoki850c-22&l=am2&o=9&a=4309227368'
           }
         ]
       }
