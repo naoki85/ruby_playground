ae44034a7f54139b80d7e2c7009bfb961f1a3a29
naoki85 <naoki.yoneyama.85@gmail.com>
2018-12-24 17:48:42 +0900

    Add asset_sync.rake

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -13,9 +13,3 @@ set :ridgepole_env, fetch(:rails_env)
 set :ridgepole_roles, :db
 set :ridgepole_schema_file, File.join(current_path, 'db/schemas', 'Schemafile')
 set :ridgepole_config_file, File.join(current_path, 'config', 'database.yml')
-
-if Rake::Task.task_defined?("webpacker:compile")
-  Rake::Task['webpacker:compile'].enhance do
-    Rake::Task["assets:sync"].invoke
-  end
-end
