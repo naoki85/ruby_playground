c38b3c07ed490888b6a0edf6a531ee4505896bda
naoki85 <naoki.yoneyama.85@gmail.com>
2019-04-09 08:00:39 +0900

    Fix post_image_path

--- app/decorators/post_decorator.rb
+++ app/decorators/post_decorator.rb
@@ -2,12 +2,10 @@ module PostDecorator
 
   # @return string
   def post_image_path(**options)
-    Rails.cache.fetch("posts/#{self.id}/image_path", expired_in: 1.days) do
-      if image_file_name.present?
-        BookRecorder::Application.config.image_base_url + image_file_name
-      else
-        'https://s3-ap-northeast-1.amazonaws.com/bookrecorder-image/commons/default_user_icon.png'
-      end
+    if image_file_name.present?
+      BookRecorder::Application.config.image_base_url + image_file_name
+    else
+      'https://s3-ap-northeast-1.amazonaws.com/bookrecorder-image/commons/default_user_icon.png'
     end
   end
 
