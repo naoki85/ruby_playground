0ab15aa8a6fd790e0126d471f79b709f32317e93
naoki85 <naoki.yoneyama.85@gmail.com>
2019-05-06 09:22:39 +0900

    Fix decorators and RecommendedBook along rubocop

--- app/models/recommended_book.rb
+++ app/models/recommended_book.rb
@@ -2,32 +2,4 @@ class RecommendedBook < ApplicationRecord
   belongs_to :post_category
 
   enum active: { draft: 0, published: 1 }
-
-  after_update -> {
-    Rails.cache.delete("recommended_books/#{self.post_category_id}")
-  }
-
-  # @param [Integer] post_category_id
-  # @return [Array]
-  def self.get_four(post_category_id)
-    Rails.cache.fetch("recommended_books/#{post_category_id}", expired_in: 1.days) do
-      r_books = get_by_category(post_category_id, 4)
-      if r_books.size < 4
-        limit = 4 - r_books.size
-        additional_r_books = get_others(post_category_id, limit)
-        r_books = r_books + additional_r_books
-      end
-      r_books
-    end
-  end
-
-  private
-
-  def self.get_by_category(category_id, limit)
-    where(post_category_id: category_id).order('id DESC').limit(limit)
-  end
-
-  def self.get_others(category_id, limit)
-    where("post_category_id != ?", category_id).order('id DESC').limit(limit)
-  end
 end
