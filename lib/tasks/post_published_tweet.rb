8df01e63970246a668cc8b38f2fa20019f6230b7
naoki85 <naoki.yoneyama.85@gmail.com>
2019-05-12 23:32:38 +0900

    Fix tweet link url

--- lib/tasks/post_published_tweet.rb
+++ lib/tasks/post_published_tweet.rb
@@ -9,7 +9,7 @@ class Tasks::PostPublishedTweet
     return if posts.empty?
     posts.each do |post|
       tweet_text = post.title + "\n"
-      tweet_text = tweet_text + BookRecorder::Application.config.blog_url + '/posts/' + post.id.to_s
+      tweet_text = tweet_text + BookRecorder::Application.config.blog_url + '/posts/' + post.id.to_s + '.html'
       twitter_client.update(tweet_text)
     end
   end
