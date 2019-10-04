eafd228fd89ec8514180d624910bf8c22a8d2a5b
naoki85 <naoki.yoneyama.85@gmail.com>
2019-10-04 23:31:02 +0900

    Add export_table_to_json

---
+++ lib/tasks/export_table_to_json.rb
@@ -0,0 +1,26 @@
+class Tasks::ExportTableToJson
+  def self.execute
+    recommended_books = RecommendedBook.where('id > 1')
+    return if recommended_books.empty?
+    result = {
+        RecommendedBooks: []
+    }
+    recommended_books.each do |book|
+      item = {
+          PutRequest: {
+              Id: { N: book.id },
+              Link: { S: book.link },
+              ImageUrl: { S: book.image_url },
+              ButtonUrl: { S: book.button_url },
+              CreatedAt: { S: book.created_at.strftime("%Y-%m-%d %H:%M:%S") },
+              UpdatedAt: { S: book.updated_at.strftime("%Y-%m-%d %H:%M:%S") }
+          }
+      }
+      result[:RecommendedBooks] << item
+    end
+
+    File.open("/var/log/recommended_books.json","w") {|file|
+      file.puts result.to_json
+    }
+  end
+end
